948deae9cb874553e3af26cdce4a35ed53be2920
Lucas Assis <lucas_dev@outlook.com.br>
2025-12-09 23:00:17 -0300

    news testes

--- teste.php
+++ teste.php
@@ -1,60 +1,84 @@
 <?php
-
-//$array = ["Ferrari", "Lambo", "GM"];
 //
-//print_r($array[2]);
-
-//$teste = isset($array[3]) ? " existe" : "não existe";
-
-//echo $teste;
-
-//$a = 0;
+////$array = ["Ferrari", "Lambo", "GM"];
+////
+////print_r($array[2]);
 //
-//if ($a == 0){
-//    $a++;
-//    echo $a;
+////$teste = isset($array[3]) ? " existe" : "não existe";
+//
+////echo $teste;
+//
+////$a = 0;
+////
+////if ($a == 0){
+////    $a++;
+////    echo $a;
+////}
+//
+////for ($i=0; $i < 10; $i++) {
+////    echo "numero " . $i . "<br>";
+////}
+//
+//// $array = [7,2,5,6,9];
+//// $quantidade = count($array);
+////
+////
+////// var_dump($quantidade);
+////
+//// for ($i = 0; $i < $quantidade - 1; $i++) {                 //( i = 1)
+////     for ($posicaoInicial = 0; $posicaoInicial < $quantidade - 1 -$i; $posicaoInicial++) {
+////
+////         $proximaPosicao = $posicaoInicial+1;
+////
+////         if ($array[$proximaPosicao] < $array[$posicaoInicial]) {
+////             $temp = $array[$posicaoInicial];
+////             $array[$posicaoInicial] = $array[$proximaPosicao];
+////             $array[$proximaPosicao] = $temp;
+////
+////         }
+////     }
+//// }
+////print_r($array);
+//
+////$array = [7,2,5,6,9];
+////sort($array);
+////print_r($array);
+//
+////$arr = [5, 3, 6, 8, 7, 9, 1, 2, 3];
+////
+////foreach ($arr as $value) {
+////    echo $value . "\n";
+////}
+//
+///*$arr = [5, 4, 6, 8, 7, 9, 1, 2, 3];
+//sort($arr);
+//foreach ($arr as $key => $value) {
+//    echo "Indice: " . ($key+1) . " valor => " . $value . "<br>";
+//}*/
+//
+////$numero = readline("Digite um numero inteiro: ");
+////$soma = 0;
+////
+////for ($i = 0; $i <= $numero; $i++) {
+////    if ($i % 3 == 0) {
+////        continue;
+////    }
+////    $soma += $i;
+////}
+////echo "Soma dos números de 1 a ". $numero .", exceto os divisíveis por 3: " . $soma;
+//
+//
+//$arr = [];
+//echo "tamanho array? ";
+//$sc = readline();
+//
+//for ($i = 0; $i < $sc; $i++) {
+//    echo "digite o " . ($i + 1) . "° nome: ";
+//    $nome = readline();
+//    $arr[] = $nome;
 //}
-
-//for ($i=0; $i < 10; $i++) {
-//    echo "numero " . $i . "<br>";
-//}
-
-// $array = [7,2,5,6,9];
-// $quantidade = count($array);
+//
+//echo "você digitou os nomes: ";
+//print_r($arr);
 //
 //
-//// var_dump($quantidade);
-//
-// for ($i = 0; $i < $quantidade - 1; $i++) {                 //( i = 1)
-//     for ($posicaoInicial = 0; $posicaoInicial < $quantidade - 1 -$i; $posicaoInicial++) {
-//
-//         $proximaPosicao = $posicaoInicial+1;
-//
-//         if ($array[$proximaPosicao] < $array[$posicaoInicial]) {
-//             $temp = $array[$posicaoInicial];
-//             $array[$posicaoInicial] = $array[$proximaPosicao];
-//             $array[$proximaPosicao] = $temp;
-//
-//         }
-//     }
-// }
-//print_r($array);
-
-//$array = [7,2,5,6,9];
-//sort($array);
-//print_r($array);
-
-//$arr = [5, 3, 6, 8, 7, 9, 1, 2, 3];
-//
-//foreach ($arr as $value) {
-//    echo $value . "\n";
-//}
-
-/*$arr = [5, 4, 6, 8, 7, 9, 1, 2, 3];
-sort($arr);
-foreach ($arr as $key => $value) {
-    echo "Indice: " . ($key+1) . " valor => " . $value . "<br>";
-}*/
-
-
-
