b398c419f622c0cf0b9365bb189fdabddffe639b
unknown <lucas_dev@outlook.com.br>
2025-12-09 21:01:53 -0300

    sintax

---
+++ teste.php
@@ -0,0 +1,60 @@
+<?php
+
+//$array = ["Ferrari", "Lambo", "GM"];
+//
+//print_r($array[2]);
+
+//$teste = isset($array[3]) ? " existe" : "não existe";
+
+//echo $teste;
+
+//$a = 0;
+//
+//if ($a == 0){
+//    $a++;
+//    echo $a;
+//}
+
+//for ($i=0; $i < 10; $i++) {
+//    echo "numero " . $i . "<br>";
+//}
+
+// $array = [7,2,5,6,9];
+// $quantidade = count($array);
+//
+//
+//// var_dump($quantidade);
+//
+// for ($i = 0; $i < $quantidade - 1; $i++) {                 //( i = 1)
+//     for ($posicaoInicial = 0; $posicaoInicial < $quantidade - 1 -$i; $posicaoInicial++) {
+//
+//         $proximaPosicao = $posicaoInicial+1;
+//
+//         if ($array[$proximaPosicao] < $array[$posicaoInicial]) {
+//             $temp = $array[$posicaoInicial];
+//             $array[$posicaoInicial] = $array[$proximaPosicao];
+//             $array[$proximaPosicao] = $temp;
+//
+//         }
+//     }
+// }
+//print_r($array);
+
+//$array = [7,2,5,6,9];
+//sort($array);
+//print_r($array);
+
+//$arr = [5, 3, 6, 8, 7, 9, 1, 2, 3];
+//
+//foreach ($arr as $value) {
+//    echo $value . "\n";
+//}
+
+/*$arr = [5, 4, 6, 8, 7, 9, 1, 2, 3];
+sort($arr);
+foreach ($arr as $key => $value) {
+    echo "Indice: " . ($key+1) . " valor => " . $value . "<br>";
+}*/
+
+
+
